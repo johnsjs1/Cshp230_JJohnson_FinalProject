e88e6c2747a62fec90a9703ad1327273d4fafff2
Jessica Johnson <johnsjs1@uw.edu>
2018-05-24 14:00:39 -0700

    Added a class class skeleton.

---
+++ Cshp230_JJohnson_FinalProject/App_Code/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cshp230_JJohnson_FinalProject.App_Code
+{
+    public class Class
+    {
+    }
+}
