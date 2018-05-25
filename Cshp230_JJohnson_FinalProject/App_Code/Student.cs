8970d97f3ec4b4ed69e69148efa2ddd32be29069
Jessica Johnson <johnsjs1@uw.edu>
2018-05-24 19:56:44 -0700

    Added C# classes

---
+++ Cshp230_JJohnson_FinalProject/App_Code/Student.cs
@@ -0,0 +1,38 @@
+﻿//Jessica Johnson 24 May 2018
+//Cshp DL230 Final Project
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class Student : IComparable<Student>
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+
+    public int CompareTo(Student other)
+    {
+        return String.Compare(this.Login, other.Login);
+    }
+    public override string ToString()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Student(int id, string name, string email, string login, string password)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Login = login;
+        Password = password;
+    }
+}
+
+public class Students : List<Student> { }
+
